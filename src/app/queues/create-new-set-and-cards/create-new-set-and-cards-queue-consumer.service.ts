529ab79c96b2baff2612112ba11d853ba5616506
João  Lima <joao.lima@somoseducacao.com.br>
2023-05-27 22:47:19 -0300

    feat: added verification to added set only if has an card

--- src/app/queues/create-new-set-and-cards/create-new-set-and-cards-queue-consumer.service.ts
+++ src/app/queues/create-new-set-and-cards/create-new-set-and-cards-queue-consumer.service.ts
@@ -14,23 +14,27 @@ export class CreateNewSetAndCardsQueueConsumer {
   @Process('create-new-set-and-cards-job')
   async createNewSetAndCardsJob(job: Job<NewSet>) {
     const { data: set } = job;
-    const newSet = MessagingSetMapper.toDomain(set);
-    try {
-      const setAlreadyExists = await this.setsRepository.getById(newSet.id);
-      if (setAlreadyExists) {
-        this.logger.warn(
-          `O set ${newSet.code} não foi criado pois ele já está cadastrado no banco de dados`,
+    if (set.cards.length) {
+      const newSet = MessagingSetMapper.toDomain(set);
+      try {
+        const setAlreadyExists = await this.setsRepository.getById(newSet.id);
+        if (setAlreadyExists) {
+          this.logger.warn(
+            `O set ${newSet.code} não foi criado pois ele já está cadastrado no banco de dados`,
+          );
+          return;
+        }
+        this.logger.log(
+          `Inserindo a coleção ${newSet.code} e ${newSet.cards.length} novas cartas`,
         );
-        return;
+        await this.setsRepository.createSetAndCards(newSet);
+      } catch (error) {
+        this.logger.error(
+          `Error when try create set ${set.code}. Error: ${error}`,
+        );
       }
-      this.logger.log(
-        `Inserindo a coleção ${newSet.code} e ${newSet.cards.length} novas cartas`,
-      );
-      await this.setsRepository.createSetAndCards(newSet);
-    } catch (error) {
-      this.logger.error(
-        `Error when try create set ${set.code}. Error: ${error}`,
-      );
+    } else {
+      this.logger.warn(`Skipped creation of set ${set.code}. Set has no cards`);
     }
   }
 }
